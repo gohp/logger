6163cbcf556bf57d05de2ba01272ac4992d6f235
jam <jamwong994@gmail.com>
2020-04-07 10:44:49 +0800

    update

--- benchmark_test.go
+++ benchmark_test.go
@@ -1,58 +1,58 @@
 package logger
 
-import (
-	logger "github.com/wzyonggege/logger"
-	"testing"
-)
-
-
-func BenchmarkLogger(b *testing.B)  {
-	b.Logf("Logging at a disabled level with some accumulated context.")
-	b.Run("logde logger without fields", func(b *testing.B) {
-		c := logger.New()
-		c.CloseConsoleDisplay()
-		c.InitLogger()
-		b.ResetTimer()
-		b.RunParallel(func(pb *testing.PB) {
-			for pb.Next() {
-				logger.Info("1234")
-			}
-		})
-	})
-	b.Run("logde logger with fields", func(b *testing.B) {
-		c := logger.New()
-		c.CloseConsoleDisplay()
-		c.InitLogger()
-		b.ResetTimer()
-		b.RunParallel(func(pb *testing.PB) {
-			for pb.Next() {
-				logger.Info("1234", logger.With("Trace", "1234455"))
-			}
-		})
-	})
-	b.Run("logde logger without fields write into file", func(b *testing.B) {
-		c := logger.New()
-		c.CloseConsoleDisplay()
-		c.SetInfoFile("../logs/test_stdout.log")
-		c.InitLogger()
-		b.ResetTimer()
-		b.RunParallel(func(pb *testing.PB) {
-			for pb.Next() {
-				logger.Info("1234")
-			}
-		})
-	})
-	b.Run("logde logger with fields write into file", func(b *testing.B) {
-		c := logger.New()
-		c.CloseConsoleDisplay()
-		c.SetInfoFile("../logs/test_stdout.log")
-		c.InitLogger()
-		b.ResetTimer()
-		b.RunParallel(func(pb *testing.PB) {
-			for pb.Next() {
-				logger.Info("1234", logger.With("Trace", "1234455"))
-			}
-		})
-	})
-}
+//import (
+//	logger "github.com/wzyonggege/logger"
+//	"testing"
+//)
+//
+//
+//func BenchmarkLogger(b *testing.B)  {
+//	b.Logf("Logging at a disabled level with some accumulated context.")
+//	b.Run("logde logger without fields", func(b *testing.B) {
+//		c := logger.New()
+//		c.CloseConsoleDisplay()
+//		c.InitLogger()
+//		b.ResetTimer()
+//		b.RunParallel(func(pb *testing.PB) {
+//			for pb.Next() {
+//				logger.Info("1234")
+//			}
+//		})
+//	})
+//	b.Run("logde logger with fields", func(b *testing.B) {
+//		c := logger.New()
+//		c.CloseConsoleDisplay()
+//		c.InitLogger()
+//		b.ResetTimer()
+//		b.RunParallel(func(pb *testing.PB) {
+//			for pb.Next() {
+//				logger.Info("1234", logger.With("Trace", "1234455"))
+//			}
+//		})
+//	})
+//	b.Run("logde logger without fields write into file", func(b *testing.B) {
+//		c := logger.New()
+//		c.CloseConsoleDisplay()
+//		c.SetInfoFile("../logs/test_stdout.log")
+//		c.InitLogger()
+//		b.ResetTimer()
+//		b.RunParallel(func(pb *testing.PB) {
+//			for pb.Next() {
+//				logger.Info("1234")
+//			}
+//		})
+//	})
+//	b.Run("logde logger with fields write into file", func(b *testing.B) {
+//		c := logger.New()
+//		c.CloseConsoleDisplay()
+//		c.SetInfoFile("../logs/test_stdout.log")
+//		c.InitLogger()
+//		b.ResetTimer()
+//		b.RunParallel(func(pb *testing.PB) {
+//			for pb.Next() {
+//				logger.Info("1234", logger.With("Trace", "1234455"))
+//			}
+//		})
+//	})
+//}
 
